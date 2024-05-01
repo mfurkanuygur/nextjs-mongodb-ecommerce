7e0fd253cc765ac26444a44ea9d9af164f89bbe1
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-05-01 23:00:24 +0300

    update banner

--- my-app/src/components/banner/HomepageBanner.jsx
+++ my-app/src/components/banner/HomepageBanner.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import banner2 from '../../../public/banner2.jpg'
+import banner1 from '../../../public/banner1.jpg'
 const HomepageBanner = () => {
     return (
-        <div className="flex flex-col-reverse md:flex-row w-full items-center py-12 md:px-12 md:py-24 gap-8 bg-main-color rounded-lg shadow-lg shadow-main-color  ">
+        <div className="flex flex-col-reverse md:flex-row w-full items-center py-12 md:px-12 md:py-24 gap-4 md:gap-8 bg-main-color rounded-lg shadow-lg shadow-main-color  ">
             <div className=" md:w-1/2 flex flex-col text-center md:text-left gap-4 ">
                 <h1 className=" text-3xl md:text-5xl text-text-main-color font-bold  ">
                     <div className="text-3xl md:text-5xl text-primary-red my-3">
@@ -12,7 +12,7 @@ const HomepageBanner = () => {
                     <div className="bg-white rounded-lg mt-1 py-1 mx-8 md:mx-0">We&rsquo;re here for you</div>
 
                 </h1>
-                <h4 className="text-center md:text-justify  leading-5 px-5 md:px-0 text-white">
+                <h4 className="text-justify  leading-5 px-5 md:px-0 text-white">
                     Keeping customer satisfaction at the forefront, we cater to every need
                     with our wide range of products. We turn shopping into an enjoyable
                     experience with our user-friendly interface and fast delivery service.
@@ -22,9 +22,9 @@ const HomepageBanner = () => {
                     improving and keeping up with innovations
                 </h4>
             </div>
-            <div className="relative md:w-1/2 bg-main-color flex justify-center" >
+            <div className="relative md:w-1/2 bg-main-color flex justify-center p-4" >
                 <Image
-                    src={banner2}
+                    src={banner1}
                     // src="/banner2.jpg"
                     width={500}
                     height={500}
